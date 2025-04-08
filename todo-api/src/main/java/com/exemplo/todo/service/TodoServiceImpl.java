d7f41a1781366221c03dbedff750735764199b93
Vinicius Batista <dev.vinicius-batista@hotmail.com>
2025-04-08 11:07:08 -0300

    feat: implementa classe TodoServiceImpl com estrutura inicial

--- todo-api/src/main/java/com/exemplo/todo/service/TodoServiceImpl.java
+++ todo-api/src/main/java/com/exemplo/todo/service/TodoServiceImpl.java
@@ -1,4 +1,33 @@
 package com.exemplo.todo.service;
 
-public class TodoServiceImpl {
+import com.exemplo.todo.entity.Todo;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TodoServiceImpl implements TodoService{
+    @Override
+    public List<Todo> listarTodos() {
+        return List.of();
+    }
+
+    @Override
+    public Todo criarTodo(Todo todo) {
+        return null;
+    }
+
+    @Override
+    public Optional<Todo> buscarPorId(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Todo atualizarTodo(Long id, Todo todo) {
+        return null;
+    }
+
+    @Override
+    public void deletarTodo(Long id) {
+
+    }
 }
