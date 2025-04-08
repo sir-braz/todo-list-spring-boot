685fb3ef193884713c42e12764033cd5f206364e
Vinicius Batista <dev.vinicius-batista@hotmail.com>
2025-04-08 09:22:51 -0300

    criação de TodoRepository para persistência com JPA

--- todo-api/src/main/java/com/exemplo/todo/repository/TodoRepository.java
+++ todo-api/src/main/java/com/exemplo/todo/repository/TodoRepository.java
@@ -1,4 +1,9 @@
 package com.exemplo.todo.repository;
 
-public interface TodoRepository {
+import com.exemplo.todo.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
