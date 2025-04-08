221e9f72368f9b207b4595d29377ea1291b194a2
Vinicius Batista <dev.vinicius-batista@hotmail.com>
2025-04-08 10:52:43 -0300

    feat: cria interface TodoService com métodos de CRUD

--- todo-api/src/main/java/com/exemplo/todo/service/TodoService.java
+++ todo-api/src/main/java/com/exemplo/todo/service/TodoService.java
@@ -1,4 +1,19 @@
 package com.exemplo.todo.service;
 
+import com.exemplo.todo.entity.Todo;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TodoService {
+
+    List<Todo> listarTodos();
+
+    Todo criarTodo(Todo todo);
+
+    Optional<Todo> buscarPorId(Long id);
+
+    Todo atualizarTodo(Long id, Todo todo);
+
+    void deletarTodo(Long id);
 }
