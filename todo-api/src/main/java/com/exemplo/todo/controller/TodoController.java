bf34a8dda14c5c55837ce0c31359bba25d4be745
Vinicius Batista <dev.vinicius-batista@hotmail.com>
2025-04-08 11:28:30 -0300

    feat(controller): cria estrutura inicial do TodoController com injeção de dependência

--- todo-api/src/main/java/com/exemplo/todo/controller/TodoController.java
+++ todo-api/src/main/java/com/exemplo/todo/controller/TodoController.java
@@ -1,4 +1,19 @@
 package com.exemplo.todo.controller;
 
+
+import com.exemplo.todo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tarefas")
 public class TodoController {
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
 }
