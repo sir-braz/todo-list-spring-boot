a2a1e006b2c8a582197be352b030d3377cfddd33
Vinicius Batista <dev.vinicius-batista@hotmail.com>
2025-04-08 11:14:58 -0300

    feat: adiciona exceção personalizada BackendException

--- todo-api/src/main/java/com/exemplo/todo/utility/BackendException.java
+++ todo-api/src/main/java/com/exemplo/todo/utility/BackendException.java
@@ -1,4 +1,12 @@
 package com.exemplo.todo.utility;
 
-public class BackendException {
+public class BackendException extends RuntimeException {
+
+    public BackendException(String message) {
+        super(message);
+    }
+
+    public BackendException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
