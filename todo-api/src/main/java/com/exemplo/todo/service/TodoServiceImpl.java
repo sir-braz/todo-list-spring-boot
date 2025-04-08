2a65d0172d08f9f798c8d90625287893a5d3fe3c
Vinicius Batista <dev.vinicius-batista@hotmail.com>
2025-04-08 11:19:09 -0300

    feat(service): implementa método listarTodos na TodoServiceImpl

--- todo-api/src/main/java/com/exemplo/todo/service/TodoServiceImpl.java
+++ todo-api/src/main/java/com/exemplo/todo/service/TodoServiceImpl.java
@@ -19,7 +19,7 @@ public class TodoServiceImpl implements TodoService{
 
     @Override
     public List<Todo> listarTodos() {
-        return List.of();
+        return todoRepository.findAll();
     }
 
     @Override
